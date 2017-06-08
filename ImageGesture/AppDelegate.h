d63fdbfc4fc3c5c970cd9a5905699f1a7cb2ba2a
zhenglin1988 <zhenglin@zhenglin1988s-mac-mini-2.local>
2017-06-08 16:56:56 +0800

    Initial Commit

---
+++ ImageGesture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageGesture
+//
+//  Created by zhenglin1988 on 17/6/8.
+//  Copyright © 2017年 郑麟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageGesture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageGesture
+//
+//  Created by zhenglin1988 on 17/6/8.
+//  Copyright © 2017年 郑麟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
